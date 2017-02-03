acb2373f94863683dffb1191e99bfe6e0dd3e439
David Jaimes <davidjaimesdesign@gmail.com>
2017-02-03 08:36:25 -0800

    finished lesson 1 moving on to lesson 2

---
+++ lesson1/testprog/aNewProject.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class aNewProject here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class aNewProject
+{
+    // instance variables - replace the example below with your own
+
+    /**
+     * Constructor for objects of class aNewProject
+     */
+    public aNewProject()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
